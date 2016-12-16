62bb8a16b9cce5976a788d4b0089f3d6d2cd6215
Tim Dobbins <hello@timdobbins.com>
2016-12-16 21:11:09 +0100

    update pins controller to assign user to new pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,14 +9,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
