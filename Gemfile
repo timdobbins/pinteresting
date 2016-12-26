e2bd88a4f88274f6fe9fdf7e4a581d4bd456ab03
Tim Dobbins <hello@timdobbins.com>
2016-12-26 19:32:40 +0100

    Add jQuery masonry

--- Gemfile
+++ Gemfile
@@ -7,12 +7,13 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
-gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise', '~> 4.2.0'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '< 2.0' 
+gem 'masonry-rails', '~> 0.2.0'
 
 
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Rails.application.configure do
   # Note to set to actual host name
   config.action_mailer.default_url_options = { host: 'http://dobbins-pinteresting.herokuapp.com' }
 
+  # sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+}
+
+
 end
